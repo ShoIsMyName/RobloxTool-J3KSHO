9fe140400fbc93c2d2d926bf11ee7e9dc78a0ee0
Sho07 <113521577+Sho2007@users.noreply.github.com>
2025-03-31 19:12:11 +0700

    Update J3KSHO.py

--- J3KSHO.py
+++ J3KSHO.py
@@ -1,11 +1,12 @@
 import auto_acc
 
-# auto_acc.AutoACC().CreateRandomACC()
-# auto_acc.AutoACC(user="Lucy443297152",passwd="GJ192PVMCGZ819").getCookie()
+# You can call function in this file | Just uncomment the code line under this
 
-# with open("AccountStorage.txt", "r", encoding="utf-8") as file:
-#     for line in file:
-#         Cookie = line.split(":")[2]
-#         print(Cookie,"\n")
+# 1. Auto create random account
+#auto_acc.AutoACC().CreateRandomACC()
 
-auto_acc.AutoACC().AccFollower()
+# 2. Auto get account cookie
+#auto_acc.AutoACC(user="YOUR_USERNAME",passwd="YOUR_PASSWORD").getCookie()
+
+# 3. Auto roblox account follower
+#auto_acc.AutoACC(url="YOUR_ACCOUNT_LINK_PROFILE").AccFollower()
